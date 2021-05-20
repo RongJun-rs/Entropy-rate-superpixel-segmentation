a267dcb07ac900ba2cf6496872221c100a7e93ad
Amine AMMOR <aamineammor@gmail.com>
2021-05-20 18:31:38 +0200

    added new method to compute the current cost from the added edge, and to compare it with the accumulated cost

---
+++ heapManager/totalCostComputer.py
@@ -0,0 +1,63 @@
+"""
+    this routine must be used for test in order to access if the total cost, of the current state of
+    the graph is equal to the expected value
+"""
+import numpy as np
+
+class TotalCostComputer:
+    def __init__(self,heap_updater,G,list_linked_nodes):
+        self.heap_updater = heap_updater
+        self.edges = self.heap_updater.edges
+        self.G = G
+        self.list_linked_nodes = list_linked_nodes
+        self.NA = self.heap_updater.heapMin[0].nodej.dNa + self.heap_updater.nbNodes
+
+        self.lbda = self.edges[0].lbda
+
+        self.pij, self.pji, self.muedgei, self.muedgej = self.partial_cost_computer_vectorized_idiffj()
+        self.mui,self.pii = self.partial_cost_computer_vectorized_self_loop()
+
+    def __call__(self):
+        return self.compute_cost()
+    def partial_cost_computer(self,idx):
+        edge = self.edges[idx]
+        mui = edge.nodei.mu
+        muj = edge.nodej.mu
+        pij = edge.pij
+        pji = edge.pji
+        mui * self.partial_entropy(pij)
+    def partial_cost_computer_vectorized_idiffj(self):
+        """
+        compute the values of pij , pji , mui and muj for i different of j
+        :return:
+        """
+        pij_pji_mui_muj = [(edge.pij,edge.pji,edge.nodei.mu,edge.nodej.mu) for edge in self.edges]
+        pij,pji,mui,muj = np.transpose(np.array(pij_pji_mui_muj))
+        return pij,pji,mui,muj
+    def partial_cost_computer_vectorized_self_loop(self):
+        """
+        compute the values of pii , mui for all nodes
+        :return:
+        """
+        s = list(self.G.nodes)
+        a = [(el.mu, el.prob) for el in s]
+        mui,pii = np.transpose(np.array(a))
+        return mui,pii
+
+    @staticmethod
+    def partial_entropy(el):
+        return - np.log(el) * el
+
+    def compute_H_cost(self):
+        cost_H = np.sum(self.partial_entropy(self.pij) * self.muedgei + self.partial_entropy(self.pji) * self.muedgej)
+        cost_H += np.sum(self.mui * self.partial_entropy(self.pii))
+        return cost_H
+
+    def compute_B_cost(self):
+        p = np.array([len(el) for el in self.list_linked_nodes])
+        layout_entropy = np.sum(self.partial_entropy(p))
+        cost_B = layout_entropy - self.NA
+        return cost_B
+
+    def  compute_cost(self):
+        return self.compute_H_cost() + self.lbda * self.compute_B_cost()
--- main.py
+++ main.py
@@ -10,13 +10,13 @@ except:
     from utils import allUsefulModule
     import data_sampler
     from heapManager import   heapInitializer,heap_updater
-    from network_initialization import network_initializor
+    from network_initialization import network_edge_initializor
 import utils.utils
 import numpy as np
 import skimage.measure
 from  networkx.algorithms.traversal.edgebfs import edge_bfs
 
-NetworkInitializor = network_initializor.NetworkInitializor
+NetworkEdgeInitializor = network_edge_initializor.NetworkEdgeInitializor
 dirFile = os.path.dirname(__file__)
 # TODO: read ground truth from path_seg using seg_doc.txt
 #TODO : check , efficient way to add edge to graph
@@ -37,27 +37,38 @@ class Main:
         self.img = img
         self.shape_img = self.img.shape[:2]
         self.nbNodes = np.product(self.img.shape[:2])
-        graph_init = NetworkInitializor(self.img)
+        graph_init = NetworkEdgeInitializor(self.img)
         self.edges_with_nodes = graph_init.edges_with_nodes
         self.G = graph_init.G
         self.G.add_weighted_edges_from(self.edges_with_nodes,connected = False)
 
-        self.edges_with_nodes_bis = np.array(list(self.G.edges.data()))
+        self.edges_with_nodes = np.array(list(self.G.edges.data()))
         self.heapMin = self.init_heap()
         self.heap_updater = heap_updater.HeapUpdater(self.heapMin,nbNodes = self.nbNodes)
-        self.update_heap()
+        self.edges_linked = self.update_heap()
+
+
+        self.list_linked_nodes = self.get_linked_nodes()
 
         self.labelling = self.label_nodes()
 
         self.labelling = utils.utils.blend_img_with_semgmentation_map(self.img,self.labelling)
 
+    def get_linked_nodes(self):
+        s1 = [id(el.linked_list_of_nodes) for el in self.G.nodes]
+        a, b = np.unique(s1, return_index=True)
+        s2 = np.array([el.linked_list_of_nodes for el in self.G.nodes])
+        s3 = s2[b]
+        return s3
+
     @timeit
     def update_heap(self):
         self.heap_updater.iterate_until_end()
+        return self.heap_updater.edges
     @timeit
     def init_heap(self):
         """ returns the heap val"""
-        self.heap_initializer = heapInitializer.HeapInitializer(self.edges_with_nodes_bis)
+        self.heap_initializer = heapInitializer.HeapInitializer(self.edges_with_nodes)
         return self.heap_initializer.heapMin
 
     def save(self):
@@ -66,32 +77,32 @@ class Main:
     def load(cls):
         return pickle.load(open(cls.path,"rb"))
 
+    #TODO : add mixin for saving and loading of instance of class at particular state
+    #TODO : add module to label the nodes
     def label_nodes(self):
-        s1 = [id(el.linked_list_of_nodes) for el in self.G.nodes]
-        a, b = np.unique(s1, return_index=True)
-        s2 = np.array([el.linked_list_of_nodes for el in self.G.nodes])
-        s3 = s2[b]
-        s4 = [([el1.pos for el1 in el],np.min([el1.pos for el1 in el])) for el in s3]
+        s4 = [([el1.pos for el1 in el],np.min([el1.pos for el1 in el])) for el in self.list_linked_nodes]
         s5 = []
         s6 = []
         for el in s4:
             s5.extend(el[0])
             s6.extend([el[1]] * len(el[0]))
-        #return np.array([s5,s6])
         g = np.argsort(s5)
         return np.array(s6)[g].reshape(self.shape_img)
-
+    def show_image_with_res(self):
+        plt.imshow(np.hstack((self.img, self.labelling)), cmap="gray");
+        plt.show()
 if __name__ == '__main__':
     index = 134
     path_img,path_seg = data_sampler.get_path_img_and_seg_from_id(index)
-    #img = plt.imread(path_img)[0:150,0:150]
-    img = plt.imread(path_img)
+    img = plt.imread(path_img)[:20,:20]
+    #img = plt.imread(path_img)
     img = img.astype("float32")/255.0
 
     #img = img[:50,:50]
     #import pdb;alg = pdb.runcall(Main,img)
     alg = Main(img)
-
-    #alg = Main.load()
-
     self = alg
+
+
+    from heapManager.totalCostComputer import TotalCostComputer
+    s = TotalCostComputer(alg.heap_updater, alg.G, alg.list_linked_nodes)
