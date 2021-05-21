e29479e8a7333e80c68d66e8d8ea7de79e3c603b
Amine AMMOR <aamineammor@gmail.com>
2021-05-21 15:04:15 +0200

    add a singleton class for the computation of lbda

---
+++ lbdaComputer.py
@@ -0,0 +1,15 @@
+from utils import singleton
+import numpy as np
+
+class LbdaComputer(metaclass=singleton.SingletonMeta):
+    lbdap = 0.5
+    def __init__(self,gainH,gainB,K):
+        self.gainH = gainH
+        self.gainB = gainB
+        self.K = K
+        self.beta = np.max(self.gainH)/np.max(self.gainB)
+        self.lbda = self.compute_lbda()
+
+    def compute_lbda(self):
+        lbda = self.lbdap * self.beta * self.K
+        return lbda
